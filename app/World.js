5058ae5b390820a75d4b9c2ea86113b7dc48d91a
keeny <barry@craftstudios.co.nz>
2012-12-01 21:29:07 +1300

    Migrate to FigureEight3

--- app/World.js
+++ app/World.js
@@ -5,14 +5,30 @@
  */
 define([
 
-	"app/shapes/FigureEight",
+	"app/Utils",
+	"app/shapes/FigureEight3",
 	"app/person/model/Person",
 	"app/person/animations/Walk"
 
-], function(FigureEight, Person, Walk) {
+], function(Utils, FigureEight3, Person, Walk) {
+
+	var _scene;
+	var _materials;
+	var _utils;
+
+	var _colours = {
+		lime: 0x00ff11,
+		magenta: 0xff00f0
+	};
 
 	function World(materialFactory) {
-		this.materials = materialFactory;
+		_materials = materialFactory;
+		_scene = new THREE.Scene();
+		_utils = new Utils(_scene);
+	};
+
+	World.prototype.scene = function() {
+		return _scene;
 	};
 
 	/**
@@ -20,24 +36,22 @@ define([
 	 */
 	World.prototype.addProps = function() {
 
-		var materials = {
-			skin: this.materials.skin(),
-			hair: this.materials.hair(),
+		var personMaterials = {
+			skin: _materials.skin(),
+			hair: _materials.hair(),
 
-			tshirt: this.materials.tshirt(),
-			pants: this.materials.pants(),
+			tshirt: _materials.tshirt(),
+			pants: _materials.pants(),
 
-			default: this.materials.solid()
+			default: _materials.solid()
 		};
 
-		this.person = new Person(materials, 100);
-		this.scene.add(this.person.rootObject());
+		this.person = new Person(personMaterials, 100);
+		_scene.add(this.person.rootObject());
 
 
-		this.eightPath = new FigureEight(400);
-		var eightLine = this.createLine(this.eightPath, 0x00ff11 );
-		eightLine.rotation.x = Math.PI/2;
-		this.scene.add( eightLine );
+		var walkPath = new FigureEight3(400, 200);
+		_utils.strokePath(walkPath, _colours.lime);
     };
 
 	/**
@@ -46,17 +60,11 @@ define([
     World.prototype.addLights = function() {
 		var light = new THREE.DirectionalLight( 0xffffff );
 		light.position.set( 1, 0.5, 0 ).normalize();
-		this.scene.add( light );
+		_scene.add( light );
 
 		var light = new THREE.DirectionalLight( 0xffffff, 0.5 );
 		light.position.set( - 1, - 0.5, 0 ).normalize();
-		this.scene.add( light );
-	};
-
-	World.prototype.createLine = function(shape, color) {
-		var points = shape.createPointsGeometry();
-
-		return new THREE.Line( points, new THREE.LineBasicMaterial( { color: color, linewidth: 2 } ) );
+		_scene.add( light );
 	};
 
     /**
@@ -65,32 +73,6 @@ define([
 	World.prototype.startTimeline = function() {
 		// var walkAnimation = new Walk();
 		// walkAnimation.animate(this.person);
-		
-		this.moveActorAlongPath(true);
-	};
-
-	World.prototype.moveActorAlongPath = function(orientToPath) {
-		var timeline = new TimelineMax({
-			repeat: -1
-		});
-
-		timeline.insert(
-			TweenMax.to(this.person.centre, 20,
-				{ onUpdate: function(app, tween){
-					var pathPosition = app.eightPath.getPoint(tween.ratio);
-					
-					app.person.centre.position.x = pathPosition.x;
-					app.person.centre.position.z = pathPosition.y;
-
-					if(orientToPath===true) {
-						var tangent = app.eightPath.getTangent(tween.ratio);
-						var angle = Math.atan2(-tangent.y, tangent.x);
-						app.person.centre.rotation.y = angle;
-					}
-
-				}, onUpdateParams:[this, "{self}"], ease: Linear.easeNone }
-			)
-		);
 	};
 
     return World;
