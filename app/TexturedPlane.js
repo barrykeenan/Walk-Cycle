a01d1a2a3fdf71141f15214be869a4ce45ba7289
keeny <barry@craftstudios.co.nz>
2012-12-31 13:11:24 +1300

    TexturedPlane component

---
+++ app/TexturedPlane.js
@@ -0,0 +1,29 @@
+/**
+ * TexturedPlane @extends THREE.Mesh
+ * 
+ * @type {Skybox}
+ */
+define([
+
+	"three.js/three.min"
+
+], function() {
+
+	function TexturedPlane(texture_url) {
+		var texture = THREE.ImageUtils.loadTexture(texture_url);
+		var geometry = new THREE.PlaneGeometry( 16000, 16000 );
+		var material = new THREE.MeshPhongMaterial( { color: 0xffffff, map: texture, perPixel: true } );
+
+		THREE.Mesh.call(this, geometry, material);
+
+		// config
+		this.material.map.repeat.set( 64, 64 );
+		this.material.map.wrapS = this.material.map.wrapT = THREE.RepeatWrapping;
+		this.rotation.x = - Math.PI/2;
+		this.receiveShadow = true;
+	};
+
+	TexturedPlane.prototype = Object.create( THREE.Mesh.prototype );
+
+	return TexturedPlane;
+});
