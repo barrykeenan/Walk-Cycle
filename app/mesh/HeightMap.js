144acf30b4161ca571fc485903e549ebf8c5b60a
keeny <barry@craftstudios.co.nz>
2013-01-02 11:36:42 +1300

    Refactor heightmap mesh component

--- app/mesh/HeightMap.js
+++ app/mesh/HeightMap.js
@@ -10,43 +10,85 @@ define([
 
 ], function() {
 
-	function HeightMap(texture_url) {
+	/**
+	 * Constructor
+	 * @param {[type]} heightmap_url Required heightmap texture
+	 * @param {[type]} options       Optional textures for additional detail
+	 */
+	function HeightMap(heightmap_url, options) {
 
-        var heightMap = THREE.ImageUtils.loadTexture('textures/terrain/hills4.png');
-		var normalMapTexture = THREE.ImageUtils.loadTexture( "textures/terrain/grasslight-big-nm.jpg");
-
-        var baseTexture = THREE.ImageUtils.loadTexture("textures/terrain/grasslight-big.jpg");
-        var diffuseTexture2 = THREE.ImageUtils.loadTexture("textures/terrain/backgrounddetailed6.jpg");
-
- 		normalMapTexture.wrapS = normalMapTexture.wrapT = THREE.RepeatWrapping;
- 		baseTexture.wrapS = baseTexture.wrapT = THREE.RepeatWrapping;
-        diffuseTexture2.wrapS = diffuseTexture2.wrapT = THREE.RepeatWrapping;
+		this.loadTextures(heightmap_url, options);
 
         var terrainShader = THREE.ShaderTerrain[ "terrain" ];
         var uniformsTerrain = THREE.UniformsUtils.clone(terrainShader.uniforms);
 
-        // the displacement determines the height of a vector, mapped to the heightmap
-        uniformsTerrain[ "tDisplacement" ].value = heightMap;
+		this.setDisplacement(uniformsTerrain, this.heightMap, this.normalMapTexture);
+		this.setOverlay(uniformsTerrain); // options
+
+        var finalHeightMapMaterial = this.getMaterial(uniformsTerrain, terrainShader);
+
+		THREE.Mesh.call(this, this.getPlaneGeometry(), finalHeightMapMaterial);
+
+		// config
+		this.rotation.x = - Math.PI/2;
+	};
+
+	HeightMap.prototype = Object.create( THREE.Mesh.prototype );
+
+	// private
+
+	/**
+	 * Load textures from urls
+	 * @param  {[type]} heightmap_url Required heightmap texture
+	 * @param  {[type]} options       Optional textures
+	 */
+	HeightMap.prototype.loadTextures = function(heightmap_url, options) {
+        this.heightMap = THREE.ImageUtils.loadTexture('textures/terrain/hills4.png');
+       	this.normalMapTexture = THREE.ImageUtils.loadTexture( "textures/terrain/grasslight-big-nm.jpg");
+
+        this.diffuseTexture1 = THREE.ImageUtils.loadTexture("textures/terrain/grasslight-big.jpg");
+        this.diffuseTexture2 = THREE.ImageUtils.loadTexture("textures/terrain/backgrounddetailed6.jpg");
+
+ 		this.normalMapTexture.wrapS = this.normalMapTexture.wrapT = THREE.RepeatWrapping;
+ 		this.diffuseTexture1.wrapS = this.diffuseTexture1.wrapT = THREE.RepeatWrapping;
+        this.diffuseTexture2.wrapS = this.diffuseTexture2.wrapT = THREE.RepeatWrapping;
+	};
+
+	/**
+	 * Displacment is the height
+	 * @param {[type]} uniformsTerrain [description]
+	 * @param {[type]} baseTexture		Required - main height map
+	 * @param {[type]} detailTexture    Optional - detail bumps?
+	 */
+	HeightMap.prototype.setDisplacement = function(uniformsTerrain, baseTexture, detailTexture) {
+        uniformsTerrain[ "tDisplacement" ].value = baseTexture;
         uniformsTerrain[ "uDisplacementScale" ].value = 400;
         // uniformsTerrain[ "uOffset" ].value.set(1, 100);
+        
+        // detail
+		uniformsTerrain[ "tDetail" ].value = detailTexture;
+	};
 
-        // normal map (bump map) detail
-		uniformsTerrain[ "tDetail" ].value = normalMapTexture;
-
-        // high texture
-        uniformsTerrain[ "tDiffuse1" ].value = baseTexture;
+	/**
+	 * Depending on how many overlay textures supplied to constructor.
+	 * Diffuse1 as a minimum
+	 */
+	HeightMap.prototype.setOverlay = function(uniformsTerrain) {
+		// high texture
+        uniformsTerrain[ "tDiffuse1" ].value = this.diffuseTexture1;
         uniformsTerrain[ "enableDiffuse1" ].value = true;
         // low texture
-        uniformsTerrain[ "tDiffuse2" ].value = diffuseTexture2;
+        uniformsTerrain[ "tDiffuse2" ].value = this.diffuseTexture2;
         uniformsTerrain[ "enableDiffuse2" ].value = true;
 
         // uniformsTerrain[ "tSpecular" ].value = normalMapTexture;
         // uniformsTerrain[ "enableSpecular" ].value = true;
 
-        // normal map
-        uniformsTerrain[ "tNormal" ].value = heightMap;
+	 	// Normal map. (Like a directional bump map)
+        uniformsTerrain[ "tNormal" ].value = this.heightMap;
         uniformsTerrain[ "uNormalScale" ].value = 3;
- 
+
+		// in the absence of any texture, can use colour        
         // uniformsTerrain[ "uDiffuseColor" ].value.setHex(0xffffff);
         // uniformsTerrain[ "uSpecularColor" ].value.setHex(0xffffff);
         // uniformsTerrain[ "uAmbientColor" ].value.setHex(0xffffff);
@@ -55,61 +97,34 @@ define([
 
         // uniformsTerrain[ "uRepeatBase" ].value.set(2, 2);
         uniformsTerrain[ "uRepeatOverlay" ].value.set(32, 32);
- 
+	};
+
+	/**
+	 * Basic plane geometry that the height map material is applied to.
+	 * @param  {[type]} length [description]
+	 * @param  {[type]} width  [description]
+	 * @return {[type]}        [description]
+	 */
+	HeightMap.prototype.getPlaneGeometry = function(length, width) {
+        var plane = new THREE.PlaneGeometry(10000, 10000, 256, 256);
+
+        plane.computeFaceNormals();
+        plane.computeVertexNormals();
+        plane.computeTangents();
+
+        return plane;
+	};
+
+	HeightMap.prototype.getMaterial = function(uniformsTerrain, terrainShader) {
         // @extends Material
-        var material = new THREE.ShaderMaterial({
+     	return new THREE.ShaderMaterial({
             uniforms: uniformsTerrain,
             vertexShader: terrainShader.vertexShader,
             fragmentShader: terrainShader.fragmentShader,
-            lights: true,
+            lights: true, // this.config object?
             fog: true
         });
- 
-        // we use a plain to render as terrain
-        var geometryTerrain = new THREE.PlaneGeometry(10000, 10000, 256, 256);
-        // var geometryTerrain = new THREE.PlaneGeometry(8000, 8000, 256, 256);
-        geometryTerrain.computeFaceNormals();
-        geometryTerrain.computeVertexNormals();
-        geometryTerrain.computeTangents();
-
-
-		// var material = new THREE.MeshPhongMaterial( { color: 0xffffff, map: texture, perPixel: true } );
- 
-        // create a 3D object to add
-		THREE.Mesh.call(this, geometryTerrain, material);
-
-		// config
-		this.rotation.x = - Math.PI/2;
-
 	};
-
-	HeightMap.prototype = Object.create( THREE.Mesh.prototype );
-
-	// HeightMap.prototype.getHeightData = function(img) {
-	//     var canvas = document.createElement('canvas');
-	//     canvas.width = 128;
-	//     canvas.height = 128;
-	//     var context = canvas.getContext( '2d' );
-
-	//     var size = 128 * 128, data = new Float32Array( size );
-
-	//     context.drawImage(img,0,0);
-
-	//     for ( var i = 0; i < size; i ++ ) {
-	//         data[i] = 0
-	//     }
-
-	//     var imgd = context.getImageData(0, 0, 128, 128);
-	//     var pix = imgd.data;
-
-	//     var j=0;
-	//     for (var i = 0, n = pix.length; i < n; i += (4)) {
-	//         var all = pix[i]+pix[i+1]+pix[i+2];
-	//         data[j++] = all/30;
-	//     }
-
-	//     return data;
-	// };
 
 	return HeightMap;
 });
